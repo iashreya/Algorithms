7c3169cdcb0fe3c3e4843b90a0cc69004eb02c27
Shreya Anand <iashreya@hotmail.com>
2020-09-05 11:17:03 +0530

    Modified app.py to recieve and show the results of TSP.py

--- TSP/App/app.py
+++ TSP/App/app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from TSP import run
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return str(run())
+    res = run()
+    path = str(res["path"])
+    time_taken = str(res["time_taken"])
+    path_length = str(res["path_length"])
+    return render_template('main_page.html', path=path, time_taken=time_taken, path_length=path_length)
