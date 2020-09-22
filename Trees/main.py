1aaef3d3e731094eb9ac1d2e2f482b6b057dcd67
Shreya Anand <iashreya@hotmail.com>
2020-09-22 15:43:51 +0530

    Added main.py in Tree

---
+++ Trees/main.py
@@ -0,0 +1,14 @@
+from tree import BST
+
+tree = BST()
+tree.insert(9)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
+tree.insert(7)
+tree.insert(10)
+
+tree.traverse(tree.root)
